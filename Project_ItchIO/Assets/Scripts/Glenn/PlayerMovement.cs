6069eff1dee03708a2c13bdf1d47a945e7a0f785
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2020-11-23 15:08:33 +0100

    pogostonkie werkt

--- Project_ItchIO/Assets/Scripts/Glenn/PlayerMovement.cs
+++ Project_ItchIO/Assets/Scripts/Glenn/PlayerMovement.cs
@@ -1,50 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Jump")]
-     private float jumpForce;
-    [SerializeField] private float jumpCharge = 0;
-    
-
     private Rigidbody2D rb;
-    private bool jump;
-    
-  
+    [SerializeField] private float jumpTime = 1f; // how long it takes in seconds to charge the bar fully
+    [SerializeField] Image pogoChargeBar;
+    [SerializeField] private float jumpForce;
+
+    private float chargeValue; // value from 0 to 1 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
+        #region Jump
         // charge power whenever you hold down the space button
         if (Input.GetKey(KeyCode.Space))
         {
-            jumpCharge += Time.deltaTime;
+            chargeValue = Mathf.Clamp01(chargeValue + Time.deltaTime / jumpTime); // charge jump and prevent value from exceeding 1
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            jump = true;
+            rb.velocity = new Vector2(rb.velocity.x, chargeValue * jumpForce); // execute jump
+            chargeValue = 0f; // reset charge
         }
-
+        pogoChargeBar.fillAmount = chargeValue; 
+        #endregion
     }
-    private void FixedUpdate()
-    {
-        if(jump == true)
-        {
-            jumpForce = 5f * jumpCharge;
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-
-            jump = false;
-            jumpCharge = 0f;
-        }
-        if(jumpCharge > 1.5f)
-        {
-            jumpCharge = 1.5f;
-        }
-    }
+   
 }
