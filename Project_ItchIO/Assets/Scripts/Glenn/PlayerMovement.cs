e32b530b6580b89ced7b4f6c74c05868a303711b
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2020-11-24 11:23:50 +0100

    isGrounded and put playermovement and bar into te same script

--- Project_ItchIO/Assets/Scripts/Glenn/PlayerMovement.cs
+++ Project_ItchIO/Assets/Scripts/Glenn/PlayerMovement.cs
@@ -5,29 +5,31 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Jump")]
     private Rigidbody2D rb;
+    private RectTransform rt;
+    [Header("Jump")]
     [SerializeField] private float jumpTime = 1f; // how long it takes in seconds to charge the bar fully
     [SerializeField] Image pogoChargeBar;
     [SerializeField] private float jumpForce;
+    private bool isGrounded = false;
 
     private float chargeValue; // value from 0 to 1 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        rt = GetComponent<RectTransform>();
     }
 
     void Update()
     {
         #region Jump
         // charge power whenever you hold down the space button
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space)&& isGrounded)
         {
             chargeValue = Mathf.Clamp01(chargeValue + Time.deltaTime / jumpTime); // charge jump and prevent value from exceeding 1
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, chargeValue * jumpForce); // execute jump
             chargeValue = 0f; // reset charge
@@ -35,5 +37,21 @@ public class PlayerMovement : MonoBehaviour
         pogoChargeBar.fillAmount = chargeValue; 
         #endregion
     }
-   
+    #region IsGrounded
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == ("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    #endregion
+
 }
