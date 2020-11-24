42314602fef94fad574a227ed76a13926117cd68
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2020-11-24 12:13:52 +0100

    pogostick position fixed

---
+++ Project_ItchIO/Assets/Scripts/Glenn/PogoStickPointer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PogoStickPointer : MonoBehaviour
+{
+  
+    void Update()
+    {
+        PogoStickMover();
+    }
+    void PogoStickMover()
+    {
+        transform.localPosition = new Vector2(0, 1.5f);
+        print(transform.localPosition.y);
+    }
+    
+}
