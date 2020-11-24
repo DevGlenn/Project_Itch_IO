f43b738f8e40457fa0bb4c6b30e4d3428675ddfc
529944 <45875335+529944@users.noreply.github.com>
2020-11-24 13:53:33 +0100

    Buttons & main scene renamed

---
+++ Project_ItchIO/Assets/Scripts/Chenoa/ButtonMan.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMan : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (SceneManager.sceneCount == 1)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
