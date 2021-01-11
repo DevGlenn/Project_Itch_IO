315c3b775563aa9bcc49574aee0e4dd22503b405
529944 <45875335+529944@users.noreply.github.com>
2021-01-11 15:08:26 +0100

    Vliegende draak animatie is werkend als hij dood gaat
    De vliegende draak gaat dood als je op hem springt, dan wordt de animatie ook afgespeeld.

--- Project_ItchIO/Assets/Scripts/Chenoa/Enemy.cs
+++ Project_ItchIO/Assets/Scripts/Chenoa/Enemy.cs
@@ -7,24 +7,39 @@ public class Enemy : MonoBehaviour
     public int hp = 1;
     public GameObject player;
 
+    private FlyingEnemy flyingEnemyScript;
+    private Rigidbody2D flyingEnemyRigidbody2D;
+
+    public bool flyingEnemyIsDead;
+
+    public void Start()
+    {
+        flyingEnemyScript = FindObjectOfType<FlyingEnemy>();
+        flyingEnemyRigidbody2D = flyingEnemyScript.gameObject.GetComponent<Rigidbody2D>();
+        flyingEnemyIsDead = false;
+    }
+
     public void TakeHit() 
     {
         Die();
 	}
+
     private void Die()
     {
         hp = -1;
         if (hp <= 0) //als het hp kleiner is of gelijk aan 0 is
         {
-            Destroy(gameObject); //destroy het gameobject (voor nu)
-            //als we de art hebben dan moet de doodgaan animatie spelen
+            flyingEnemyIsDead = true;
+            flyingEnemyScript.animator.SetBool("IsDead", true); //speel de animatie af
+            flyingEnemyRigidbody2D.constraints &= ~RigidbodyConstraints2D.FreezePositionY; //freezed de positie op de Y as niet meer zodat hij naar beneden valt
+            flyingEnemyScript.gameObject.GetComponent<BoxCollider2D>().isTrigger = true; //maakt de draak een trigger want dan valt hij door alles heen
         }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         PlayerMovement player = collision.collider.GetComponent<PlayerMovement>(); //de player = zijn eigen collider en dan get hij het script PlayerMovement
-        if (player != null && collision.collider == player.PlayerCollider) //als de player bestaat en de de collision de player collider is
+        if (player != null && collision.collider == player.PlayerCollider && flyingEnemyIsDead == false) //als de player bestaat en de de collision de player collider is
         {
             player.TakeDamage(); //doe de TakeDamage functie van het player script
 		}
